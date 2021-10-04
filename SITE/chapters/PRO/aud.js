fc1d18ddd8429abe001acdc618e0a23690bc411c
jeevasamivel7 <jeevasamivel7@gmail.com>
2021-10-04 11:06:05 +0530

    update

--- SITE/chapters/PRO/aud.js
+++ SITE/chapters/PRO/aud.js
@@ -39,9 +39,12 @@ function aud_play_pause() {
   var myAudio = document.getElementById("myTune");
   if (myAudio.paused) {
     myAudio.play();
+    
   } else {
     myAudio.pause();
   }
 }
+
+
 
 
